4388f8aa780ede3b26b7c2bebb98baee7e390191
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2025-07-11 17:47:44 +0300

    Index

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from myapp.models import Food
+
 # Create your views here.
+def index(request):
+  foods = Food.objects.all()
+  return render(request, 'myapp/index.html', {'foods': foods})
