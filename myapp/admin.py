ef47b166c99ae5b4bbb7aab42d248c370f4a5ac5
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2025-07-11 18:39:04 +0300

    Create Consume model, save to db with user and food

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from myapp.models import Food
+from myapp.models import Consume, Food
 
 # Register your models here.
 admin.site.register(Food)
+admin.site.register(Consume)
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
-from myapp.models import Food
+from myapp.models import Consume, Food
 
 # Create your views here.
 def index(request):
+  if request.method == "POST":
+    # consumedFood = request.POST['consumedFood']
+    consumedFoodName = request.POST.get('consumedFood', '') # or this
+    food = Food.objects.get(name = consumedFoodName)
+    user = request.user
+    consume = Consume(consumedFood = food, user = user)
+    consume.save()
   foods = Food.objects.all()
   return render(request, 'myapp/index.html', {'foods': foods})
